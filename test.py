9767e2689a2ee911c61b8eadb6170ca224d84da6
Seth Erickson <sr.erickson@gmail.com>
2024-11-07 20:46:58 +0000

    better example, using beautifulsoup

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+from time import sleep
 
 options = Options()
 options.add_argument('--headless')
@@ -9,6 +12,10 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver.get("https://python.org")
-print(driver.title)
+driver.get("https://www.scrapethissite.com/pages/ajax-javascript/")
+button = driver.find_element(by=By.ID, value="2015")
+button.click()
+sleep(2)
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+print(soup.find(class_='film').prettify())
 driver.close()
